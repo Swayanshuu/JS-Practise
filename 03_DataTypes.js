558dff9ac9b5fa6cd0b4dbf96e2fac6f42089f72
Swayanshu <swayanshu19@gmail.com>
2025-02-17 22:43:45 +0530

    Data Types

--- 03_DataTypes.js
+++ 03_DataTypes.js
@@ -1,1 +1,23 @@
 //Studing DataTypes..
+
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3); console.log("shibu")  // for one line code use ';' between them.
+
+console.log ("6-4")
+console.log("shibuuuuu") // for multiline code we dont use ';'
+
+let name = "swqyanshu" // act as string type
+let age = 19 // act as int type 
+let boolean = true // act as boolean type 
+let ch = 'a' // act as character type
+ 
+
+console.table ([name,age,boolean,ch])
+
+// if we want to know the type of a perticula data type then..
+
+console.log (typeof age)
+console.log(typeof name)
+
+// so generally 'let' use as multiple data type 
