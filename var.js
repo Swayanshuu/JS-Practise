d27b019fd0b830be90bc14a449e46c5d75ff6b6e
Swayanshu <swayanshu19@gmail.com>
2025-02-17 19:28:12 +0530

    Variables

---
+++ var.js
@@ -0,0 +1,10 @@
+const RollNo = 76 //can not change as it means it is constant...
+let Email ="swayanshu19@gmail.com"
+
+city = "Nayagarh" // not recommanded
+
+let RedgNo=123;
+
+console.table([RollNo,Email,city,RedgNo])
+
+// so generally 'let' is universal to use instead of int char or string...
